32868348b32b752d7dbcad1fa23476cd0b8fc3cc
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-10 11:52:58 -0700

    User input.

--- GCF.cpp
+++ GCF.cpp
@@ -8,4 +8,8 @@ int main() {
 	using std::cin;
 	using std::cout;
 	using std::endl;
+
+	cout << "Greatest Common Factor Calculator" << endl << endl;
+	cout << "First number: ";	cin >> num1;
+	cout << "Second number: ";	cin >> num2;
 }
