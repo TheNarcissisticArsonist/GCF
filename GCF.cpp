041f9d13d62a295588a8fb4c7d6e4ff43a270999
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-10 12:21:06 -0700

    ...calculate the gcf.

--- GCF.cpp
+++ GCF.cpp
@@ -8,7 +8,7 @@ int main() {
 	int num1 = 0;
 	int num2 = 0;
 	int gcf  = 0;
-	int test = 0;
+	int test = 1;
 
 	cout << "Greatest Common Factor Calculator" << endl << endl;
 	cout << "First number: ";	cin >> num1;
@@ -18,8 +18,15 @@ int main() {
 		gcf = num1;
 	}
 	else {
-
+		while(test <= num1 && test <= num2) {
+			if(num1 % test == 0 && num2 % test == 0) {
+				gcf = test;
+			}
+			++test;
+		}
 	}
+
+	cout << "The GCF is " << gcf << "." << endl;
 
 	return 0;
 }
