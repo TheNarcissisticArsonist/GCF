1f18f285312f99a205f4af467ace980910422bd4
TheNarcissisticArsonist <TommyCohn116@gmail.com>
2015-08-10 12:11:13 -0700

    Made variables local, added int test.

--- GCF.cpp
+++ GCF.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
-int num1;
-int num2;
-int gcf;
-
 int main() {
 	using std::cin;
 	using std::cout;
 	using std::endl;
+
+	int num1;
+	int num2;
+	int gcf;
+	int test;
 
 	cout << "Greatest Common Factor Calculator" << endl << endl;
 	cout << "First number: ";	cin >> num1;
